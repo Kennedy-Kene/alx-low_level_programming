564f7fa15fd8d02914b8d2621bb45f3a5c2101a4
Kennedy-Kene <kodume2@yahoo.com>
2023-08-24 18:05:26 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-void my_initializer(void) __attribute__ ((constructor));
+void first(void) __attribute__ ((constructor));
 
 /**
-* my_initializer - a function executed before main
-*/
-void my_initializer(void)
+ * first - prints a sentence before the main
+ * function is executed
+ */
+void first(void)
 {
-	printf("Before main: Initializing something...\n");
-	printf("Hello from my_initializer!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
-
